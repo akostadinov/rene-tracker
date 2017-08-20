62f337b0d5b66cc93d4b5395af4c917a038231fe
Aleksandar Kostadinov <akostadi@redhat.com>
2017-08-20 20:41:38 +0300

    add fields for Volunteers

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -1,5 +1,15 @@
 class Volunteer
   include Mongoid::Document
+  field :name, String
+  field :username, String
+  field :country, String
+  field :city, String
+  filed :notes, String
 
-  has_many :reports
+  has_many :reports, dependent: :destroy
+
+  validates :name, preysence: true, length: { minimum: 5, maximum: 100 }
+  validates :username, presence: true, length: { minimum: 3, maximum: 16 }, format: { with: /\A[-\w]+\z/ }
+  validates :country, presence: true, length: { minimum: 3, maximum: 16 }, format: { with: /\A[-\w ]+\z/ }
+  validates :city, presence: true, length: { minimum: 3, maximum: 16 }, format: { with: /\A[-\w ]+\z/ }
 end
