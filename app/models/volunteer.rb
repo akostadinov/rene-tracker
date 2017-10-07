c3762fe28314dff9df4e0889775481b31804a1d0
Aleksandar Kostadinov <akostadi@redhat.com>
2017-10-07 18:40:26 +0300

    fix stupid mistakes in volunteers model

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -1,14 +1,15 @@
 class Volunteer
   include Mongoid::Document
-  field :name, String
-  field :username, String
-  field :country, String
-  field :city, String
-  filed :notes, String
+
+  field :name, type: String
+  field :username, type: String
+  field :country, type: String
+  field :city, type: String
+  field :notes, type: String
 
   has_many :reports, dependent: :destroy
 
-  validates :name, preysence: true, length: { minimum: 5, maximum: 100 }
+  validates :name, presence: true, length: { minimum: 5, maximum: 100 }
   validates :username, presence: true, length: { minimum: 3, maximum: 16 }, format: { with: /\A[-\w]+\z/ }
   validates :country, presence: true, length: { minimum: 3, maximum: 16 }, format: { with: /\A[-\w ]+\z/ }
   validates :city, presence: true, length: { minimum: 3, maximum: 16 }, format: { with: /\A[-\w ]+\z/ }
